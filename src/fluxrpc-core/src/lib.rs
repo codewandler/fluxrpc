1016d27055f43c1c1eaf257ba9b2e6292303e3fe
Timo Friedl <timo@babelforce.com>
2025-06-17 00:20:59 +0200

    release: v0.19.0

--- src/fluxrpc-core/src/lib.rs
+++ src/fluxrpc-core/src/lib.rs
@@ -11,5 +11,5 @@ pub use transport::websocket::{
 };
 
 pub use message::*;
-pub use session::{RpcSession, RpcSessionError};
+pub use session::{RpcSession, RpcSessionError, SessionState};
 pub use typed::TypedRpcHandler;
